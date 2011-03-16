48b01eab88c1ea63757318341262a6f6f0cf17ca
maxogden <max@maxogden.com>
2011-03-15 23:46:44 -0700

    form serialization and submission

--- public/script/monocles.js
+++ public/script/monocles.js
@@ -1,8 +1,21 @@
 $(function(){
   $("label").inFieldLabels();
-  
-  $('.button').click(function(e){
-    console.log($('#user_url').val())
+
+  function provision() {
+    var data = {};
+    $(['username', 'password', 'url']).each(function(i, input) {
+      data[input] = $("#" + input).val();
+    })
     
-  })
+    $.post("/provision", data, function(data) {
+      $('body').append(JSON.stringify(data));
+    })
+    
+  }
+  
+  $("input").keydown(function(e) {
+     if(e.keyCode == 13) provision();
+  });
+
+  $('.button').click( provision );
 })
